7b5a806f2f13902268ff91d787ccd890467d585f
magnawind <magnawind@gmail.com>
2020-05-23 19:40:16 +0200

    Now following official tutorial

--- app.py
+++ app.py
@@ -1,24 +1,45 @@
-
 import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objects as go
+import csv
 
+labels = {}
+index = 0
+sources = []
+targets = []
+values = []
+with open('LCA.csv', newline='') as csvfile:
+    LCAreader = csv.reader(csvfile)
+    for row in LCAreader:
+        source = row[0]
+        target = row[1]
+        value = row[2]
+        if source not in labels:
+            labels[source] = index
+            index += 1
+        if target not in labels:
+            labels[target] = index
+            index += 1
+        sources.append(labels[source])
+        targets.append(labels[target])
+        values.append(float(value))
 
 fig = go.Figure(data=[go.Sankey(
     node = dict(
       pad = 15,
       thickness = 20,
       line = dict(color = "black", width = 0.5),
-      label = ["A1", "A2", "B1", "B2", "C1", "C2"],
-      color = "blue"
+      label = list(labels.keys()),
+      color = "red"
     ),
     link = dict(
-      source = [0, 1, 0, 2, 3, 3], # indices correspond to labels, eg A1, A2, A2, B1, ...
-      target = [2, 3, 3, 4, 4, 5],
-      value = [8, 4, 2, 8, 4, 2]
+      source = sources, # indices correspond to labels, eg A1, A2, A2, B1, ...
+      target = targets,
+      value = values
   ))])
+
 fig.update_layout(title_text="Basic Sankey Diagram", font_size=10)
 
 
