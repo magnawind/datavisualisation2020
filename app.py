9792c8bf53f1f3e9d00c72461bb08a5ce62ff8ab
magnawind <magnawind@gmail.com>
2020-05-23 16:16:22 +0200

    moved sankey out of html

--- app.py
+++ app.py
@@ -5,6 +5,23 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objects as go
 
+
+fig = go.Figure(data=[go.Sankey(
+    node = dict(
+      pad = 15,
+      thickness = 20,
+      line = dict(color = "black", width = 0.5),
+      label = ["A1", "A2", "B1", "B2", "C1", "C2"],
+      color = "blue"
+    ),
+    link = dict(
+      source = [0, 1, 0, 2, 3, 3], # indices correspond to labels, eg A1, A2, A2, B1, ...
+      target = [2, 3, 3, 4, 4, 5],
+      value = [8, 4, 2, 8, 4, 2]
+  ))])
+fig.update_layout(title_text="Basic Sankey Diagram", font_size=10)
+
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -19,19 +36,7 @@ app.layout = html.Div([
         value='LA'
     ),
     html.Div(id='display-value'),
-    go.Figure(data=[go.Sankey(
-    node = dict(
-      pad = 15,
-      thickness = 20,
-      line = dict(color = "black", width = 0.5),
-      label = ["A1", "A2", "B1", "B2", "C1", "C2"],
-      color = "blue"
-    ),
-    link = dict(
-      source = [0, 1, 0, 2, 3, 3], # indices correspond to labels, eg A1, A2, A2, B1, ...
-      target = [2, 3, 3, 4, 4, 5],
-      value = [8, 4, 2, 8, 4, 2]
-  ))])
+    dcc.Graph(figure=fig)
 ])
 
 @app.callback(dash.dependencies.Output('display-value', 'children'),
