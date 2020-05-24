d4ff19e962f35becaf2964b78f8864ff764154bc
magnawind <magnawind@gmail.com>
2020-05-24 22:05:10 +0200

    named all files correctly

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def create_chart(file):
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-fig = create_chart('LSA.csv')
+fig = create_chart('LCA.csv')
 
 app.layout = html.Div([
     html.H2('Magnus Winding'),
